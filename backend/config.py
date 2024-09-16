9e3d531014a3af3356fbb2c2a098f2d2a13a1d3b
zurgenius <96647976+zurgenius@users.noreply.github.com>
2024-09-17 02:14:20 +0300

    added backend structure

---
+++ backend/config.py
@@ -0,0 +1,35 @@
+from pydantic import PostgresDsn, BaseModel
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", case_sensitive=False)
+
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+    reset_password_token_secret: str
+    verification_token_secret: str
+
+    @property
+    def database_url_asyncpg(self):
+        # postgresql+asyncpg://postgres:postgres@localhost:5432/sa
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
+
+settings = Settings()
---
+++ backend/core/db_helper.py
@@ -0,0 +1,29 @@
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    AsyncEngine,
+    async_sessionmaker,
+)
+from config import settings
+
+
+class DataBaseHelper:
+    engine: AsyncEngine = create_async_engine(
+        url=settings.database_url_asyncpg, echo=True
+    )
+
+    session_factory = async_sessionmaker(
+        bind=engine,
+        autoflush=False,
+        autocommit=False,
+        expire_on_commit=False,
+    )
+
+    async def dispose(self):
+        await self.engine.dispose()
+
+    async def session_getter(self):
+        async with self.session_factory() as session:
+            yield session
+
+
+db_helper = DataBaseHelper()
