e79f6ed3f315de6b2bfca5611143fbdc81add0a9
zurgenius <96647976+zurgenius@users.noreply.github.com>
2025-03-07 03:10:45 +0300

    feat: rendering images for tasks in variant and task list

---
+++ frontend/src/features/singleVariant/ui/SingleTask.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import { TaskRead } from "../../../entities/task"
+import { useGetPresignedUrlForGetFromS3Query } from "../../../entities/task/api/api";
+
+export const SingleTask = (task: TaskRead) => {
+    const placeholderImage = "https://dummyimage.com/300x200/cccccc/ffffff&text=No+Image";
+    const { data: presignedUrlData } = useGetPresignedUrlForGetFromS3Query(
+            `${task.file_id}.${task.file_extension}`,
+            { skip: !task.file_id, } // Пропускаем запрос, если file_id отсутствует
+        );
+    console.log(presignedUrlData)
+    return (
+        <>
+            <div className="flex justify-center items-center w-full h-96"> {/* Контейнер для центрирования */}
+                <img
+                    key={task.file_id ? `${task.file_id}.${task.file_extension}` : "placeholder"}
+                    src={!task.file_id ? placeholderImage : presignedUrlData}
+                    alt="Card Image"
+                    className="max-w-full max-h-full object-contain" // Ограничение по ширине и высоте
+                />
+            </div>
+            <div key={task.id} className="text-center">
+                <p>Текст задания: {task.text}</p>
+                <p>Тип задания: {task.type}</p>
+            </div>
+        </>
+    )
+    }
+
--- frontend/src/features/singleVariant/ui/TaskList.tsx
+++ frontend/src/features/singleVariant/ui/TaskList.tsx
@@ -9,6 +9,8 @@ import { useAppSelector } from "../../../shared/store"
 import { selectCurrentUser } from "../../../entities/user/model/user.slice"
 import { useAppDispatсh } from "../../../shared/store"
 import { selectVariantAnswers, selectAnswerToTask, addAnswerToTask } from "../model/slice"
+import { useGetPresignedUrlForGetFromS3Query } from "../../../entities/task/api/api"
+import { SingleTask } from "./SingleTask"
 export const TaskList = () => {
     const { id } = useParams<{id: string}>()
     const { data: variant, isSuccess: isGetVariantSuccess, error: getVariantError, isError: isGetVariantError } = useGetVariantByIdWithTasksQuery(id || skipToken)
@@ -18,7 +20,7 @@ export const TaskList = () => {
     const { handleRemoveTaskFromVariant, handleCheckVariant } = useHandleSingleVariantMutations()
     const user = useAppSelector(selectCurrentUser)
     const dispatch = useAppDispatсh()
-
+    
     const answerToCurrentTask = useAppSelector((state) =>selectAnswerToTask(state,task_id))
     const answersToVariant = useAppSelector(selectVariantAnswers)
     console.log(answerToCurrentTask)
@@ -87,10 +89,7 @@ export const TaskList = () => {
                         {variant.tasks
                             .filter((task) => task.id == task_id)
                             .map((task) => (
-                                <div key={task.id} className="text-center ">
-                                    <p>Текст задания: {task.text}</p>
-                                    <p>Тип задания: {task.type}</p>
-                                </div>
+                                <SingleTask {...task} />
                             ))}
                     </div>
                     {user?.role_type == "student" &&
