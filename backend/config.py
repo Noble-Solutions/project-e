57f5d973b1b5f0c8f5650652c0487439595f405f
zurgenius <96647976+zurgenius@users.noreply.github.com>
2025-03-09 02:57:53 +0300

    fix: fixed access_token lifetime

--- backend/config.py
+++ backend/config.py
@@ -9,7 +9,7 @@ class AuthJWT(BaseModel):
     private_key_path: Path = BASE_DIR / "certs" / "jwt-private.pem"
     public_key_path: Path = BASE_DIR / "certs" / "jwt-public.pem"
     algorithm: str = "RS256"
-    access_token_expire_minutes: int = 1
+    access_token_expire_minutes: int = 5
     refresh_token_expire_minutes: int = (
         60 * 24 * 7
     )  # Длительное время жизни refresh token
